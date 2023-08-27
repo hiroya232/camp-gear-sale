a1fa253a6e51ecba7d8387df5eca8fb20c281261
hiroya-akeoka <mowoarm232@gmail.com>
2023-08-27 21:46:05 +0900

    初回実装
    ・ツイート日時&通常URLリストをjson文字列で引数として受け取り、ツイートをスケジュール実行する
    ・複数日時に対応
    ・短縮URLの作成はTinyURL（要検討）
    ・デバッグ設定を作成

--- main.py
+++ main.py
@@ -1,124 +1,125 @@
-from amazon.paapi import AmazonAPI
-
-# PA-API資格情報
-ACCESS_KEY = "***REMOVED***" #アクセスキー
-SECRET_KEY = "***REMOVED***" #シークレットキー
-ASSOCIATE_ID = "***REMOVED***" #アソシエイトID
-COUNTRY = "JP"  #日本のAmazonなので"JP"
-
-amazon_api = AmazonAPI(ACCESS_KEY, SECRET_KEY, ASSOCIATE_ID, COUNTRY )
-
-asin_list = [
-    [
-      'B00QEN7NMU',
-      'B00279LZ0G',
-      'B09QZ2TXKN',
-      'B07HRPBRBS',
-      'B07Q7SWXLZ',
-      'B0BMPZPSCL',
-      'B08563GS15',
-      'B09TNDQD56',
-      'B08FTBS3HH',
-      'B007N77TNO',
-      'B07VWGBHZW',
-      'B09TNLSN29',
-      'B0BQJ18TX5',
-      'B073PSRTCG',
-      'B000UH0ROM',
-      'B0BM46PJ9C',
-      'B07X9VZD3M',
-    ],
-]
-
-
-# ツイート投稿
+import sys
+import json
+import datetime
+import re
 import requests
-from requests_oauthlib import OAuth1
-from apscheduler.schedulers.blocking import BlockingScheduler
 
-# Twitter API資格情報
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pyshorteners import Shortener
+from amazon.paapi import AmazonAPI
+from requests_oauthlib import OAuth1
+
+# PA-API情報
+ACCESS_KEY = '***REMOVED***'
+SECRET_KEY = '***REMOVED***'
+ASSOCIATE_ID = '***REMOVED***'
+COUNTRY = 'JP'
+
+AMAZON_API = AmazonAPI(ACCESS_KEY, SECRET_KEY, ASSOCIATE_ID, COUNTRY )
+
+
+# TwitterAPI情報
+# # テスト用
+# CONSUMER_KEY = '***REMOVED***'
+# CONSUMER_SECRET = '***REMOVED***'
+# ACCESS_TOKEN = '***REMOVED***'
+# ACCESS_TOKEN_SECRET = '***REMOVED***'
+
+# 本番用
 CONSUMER_KEY = '***REMOVED***'
 CONSUMER_SECRET = '***REMOVED***'
 ACCESS_TOKEN = '***REMOVED***'
 ACCESS_TOKEN_SECRET = '***REMOVED***'
 
-# OAuth1認証
-auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+TWITTER_AUTH = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+TWITTER_API_URL = 'https://api.twitter.com/2/tweets'
 
-# Twitter API v2のエンドポイント
-endpoint_url = "https://api.twitter.com/2/tweets"
 
-import datetime
-import pytz
+def get_tweet_datetime_list(tweet_datetime):
+  return [
+    tweet_datetime.replace(hour=7, minute=0, second=0),
+    tweet_datetime.replace(hour=7, minute=30, second=0),
+    tweet_datetime.replace(hour=8, minute=0, second=0),
+    tweet_datetime.replace(hour=12, minute=0, second=0),
+    tweet_datetime.replace(hour=12, minute=30, second=0),
+    tweet_datetime.replace(hour=13, minute=0, second=0),
+    tweet_datetime.replace(hour=15, minute=0, second=0),
+    tweet_datetime.replace(hour=15, minute=30, second=0),
+    tweet_datetime.replace(hour=16, minute=0, second=0),
+    tweet_datetime.replace(hour=16, minute=30, second=0),
+    tweet_datetime.replace(hour=17, minute=0, second=0),
+    tweet_datetime.replace(hour=20, minute=0, second=0),
+    tweet_datetime.replace(hour=20, minute=30, second=0),
+    tweet_datetime.replace(hour=21, minute=0, second=0),
+    tweet_datetime.replace(hour=21, minute=30, second=0),
+    tweet_datetime.replace(hour=22, minute=0, second=0),
+  ]
 
-tokyo_tz = pytz.timezone("Asia/Tokyo")
-now_date = datetime.datetime.now(tokyo_tz)  # 現在の日時をAsia/Tokyoのタイムゾーンで取得
-print(now_date)
 
-def post_tweet(asin):
-  products = amazon_api.get_items(item_ids=[asin])
+def extract_asin_from_url(url):
+  # 通常の商品ページのURLからASINを抽出
+  match = re.search(r'/dp/(\w{10})', url)
+  if match:
+      return match.group(1)
 
-  product_title = products['data'][asin].item_info.title.display_value
-  product_discount_rate = products['data'][asin].offers.listings[0].price.savings.percentage
-  product_url = products['data'][asin].detail_page_url
+  # 別の形式のURLからASINを抽出
+  match = re.search(r'/gp/product/(\w{10})', url)
+  if match:
+      return match.group(1)
 
-    # ツイート内容の作成
-  tweet_content = f"""
+  return None
+
+
+def post_tweet(asin, shortened_url):
+  product_data = AMAZON_API.get_items(item_ids=[asin])['data'][asin]
+
+  product_title = product_data.item_info.title.display_value
+  product_discount_rate = product_data.offers.listings[0].price.savings.percentage
+
+  payload = {
+    'text': f"""
 【{product_discount_rate}%オフ！】
 
 {product_title}
 
 詳細は🔽からチェック✔
-{product_url}
+{shortened_url}
 
 #キャンプ
 #アウトドア
 #キャンプ好きと繋がりたい
-  """
-
-  payload = {
-    "text": tweet_content
+    """
   }
 
-  print(payload)
+  response = requests.post(TWITTER_API_URL, auth=TWITTER_AUTH, json=payload)
 
-  # ツイートの投稿
-  response = requests.post(endpoint_url, auth=auth, json=payload)
-
-  # レスポンスを表示
-  print(datetime.datetime.now(tokyo_tz))
+  print(asin)
   print(response.json())
 
 
-# タスクのスケジュール設定
-scheduler = BlockingScheduler(timezone="Asia/Tokyo")
+def main():
+  shortener = Shortener()
+  scheduler = BlockingScheduler(timezone='Asia/Tokyo')
 
-tomorrow_date = now_date + datetime.timedelta(days=1)
+  for index, arg in enumerate(sys.argv):
+    if index != 0:
+      persed_arg = (json.loads(arg))
+      tweet_date = persed_arg['date']
+      url_list   = persed_arg['url_list']
 
-tweet_datetime_list = [
-    now_date.replace(hour=12, minute=0, second=0),
-    now_date.replace(hour=12, minute=30, second=0),
-    now_date.replace(hour=13, minute=0, second=0),
-    now_date.replace(hour=15, minute=0, second=0),
-    now_date.replace(hour=15, minute=30, second=0),
-    now_date.replace(hour=16, minute=0, second=0),
-    now_date.replace(hour=16, minute=30, second=0),
-    now_date.replace(hour=17, minute=0, second=0),
-    now_date.replace(hour=20, minute=0, second=0),
-    now_date.replace(hour=20, minute=30, second=0),
-    now_date.replace(hour=21, minute=0, second=0),
-    now_date.replace(hour=21, minute=30, second=0),
-    now_date.replace(hour=22, minute=0, second=0),
-    now_date.replace(hour=22, minute=30, second=0),
-    now_date.replace(day=tomorrow_date.day, hour=7, minute=0, second=0),
-    now_date.replace(day=tomorrow_date.day, hour=7, minute=30, second=0),
-    now_date.replace(day=tomorrow_date.day, hour=8, minute=0, second=0),
-]
+      tweet_datetime = get_tweet_datetime_list(datetime.datetime.strptime(tweet_date, '%Y-%m-%d'))
 
-for i, asin_daily in enumerate(asin_list):
-  for asin, tweet_datetime  in zip(asin_daily, tweet_datetime_list):
-    if i != 0:
-      tweet_datetime += datetime.timedelta(days=1)
-    scheduler.add_job(post_tweet, 'date', run_date=tweet_datetime, args=[asin])
+      asin_list          = []
+      shortened_url_list = []
+      for url in url_list:
+        asin_list.append(extract_asin_from_url(url))
+        shortened_url_list.append(shortener.tinyurl.short(url))
 
-scheduler.start()
+      for asin, shortened_url, tweet_datetime in zip(asin_list, shortened_url_list, tweet_datetime):
+        if asin is not None:
+          scheduler.add_job(post_tweet, 'date', run_date=tweet_datetime, args=[asin, shortened_url])
+
+  scheduler.start()
+
+if __name__ == '__main__':
+    main()
