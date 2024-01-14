47839787dbfb14f61ed95dc2b47497bccb4659d2
hiroya-akeoka <mowoarm232@gmail.com>
2024-01-14 12:10:12 +0900

    :sparkles:ツイート処理に関するテストコードを作成

---
+++ src/tests/test_tweet.py
@@ -0,0 +1,37 @@
+import unittest
+
+from tweet import create_content
+
+
+class TestTweet(unittest.TestCase):
+    def test_create_content(self):
+        test_case = {
+            "discount_rate": 10,
+            "product_title": "テスト商品1",
+            "short_url": "http://tinyurl.com/test",
+            "expected": {
+                "text": f"""
+【10%オフ！】
+
+テスト商品1
+
+詳細は🔽からチェック✔
+http://tinyurl.com/test
+
+#キャンプ
+#アウトドア
+#キャンプ好きと繋がりたい
+        """
+            },
+        }
+
+        result = create_content(
+            test_case["discount_rate"],
+            test_case["product_title"],
+            test_case["short_url"],
+        )
+        self.assertEqual(result, test_case["expected"])
+
+
+if __name__ == "__main__":
+    unittest.main()
