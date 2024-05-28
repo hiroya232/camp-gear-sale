c40d3fbb0823a28c066fc2b2ddef7d08f6a2f993
hiroya-akeoka <mowoarm232@gmail.com>
2024-05-28 07:09:23 +0000

    :recycle:DDDに沿ってリファクタリング（改善の余地あり）

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import sys
+
+
 sys.path.append("src")
 
 from dotenv import load_dotenv
 
-from application.scheduler import add_job, start_scheduler
-from domain.tweet import post_tweet
+from application.usecase.post_camp_gear_sale_use_case import PostCampGearSaleUseCase
+from infrastructure.repository.product_repository import ProductRepository
+from infrastructure.service.product_service import ProductService
+from infrastructure.service.post_service import PostService
+
+
+load_dotenv()
 
 
 def main():
-    load_dotenv()
+    product_repository = ProductRepository(ProductService())
+    post_service = PostService()
 
-    add_job(post_tweet)
-
-    start_scheduler(post_tweet)
+    post_camp_gear_sale_use_case = PostCampGearSaleUseCase(
+        product_repository, post_service
+    )
+    post_camp_gear_sale_use_case.handle()
 
 
 if __name__ == "__main__":
