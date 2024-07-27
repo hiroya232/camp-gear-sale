b13aca2f0f4c47368b6cd1677a966872f157fc6f
hiroya-akeoka <mowoarm232@gmail.com>
2024-07-27 19:27:31 +0900

    :fire:AWS移行に伴いスケジューリングに関するコードを削除
    EventBridgeSchedulerで設定するため

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 from dotenv import load_dotenv
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 from application.usecase.post_camp_gear_sale_use_case import PostCampGearSaleUseCase
 from infrastructure.repository.product_repository import ProductRepository
@@ -22,9 +21,7 @@ def main():
         product_repository, post_service
     )
 
-    scheduler = BlockingScheduler(timezone="Asia/Tokyo")
-    scheduler.add_job(post_camp_gear_sale_use_case.handle, "interval", minutes=30)
-    scheduler.start()
+    post_camp_gear_sale_use_case.handle()
 
 
 if __name__ == "__main__":
