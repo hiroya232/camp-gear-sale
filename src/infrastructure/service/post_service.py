9b12de1a5237c34fecc5eb6df8e75d9b31a95e94
hiroya-akeoka <mowoarm232@gmail.com>
2024-12-08 21:56:31 +0900

    :sparkles:XとThreadsでトラッキングIDを別々に設定できるように修正
    ThreadsもX用のトラッキングIDでポストしていたため別々に集計できるようにした

--- src/infrastructure/service/post_service.py
+++ src/infrastructure/service/post_service.py
@@ -25,7 +25,7 @@ class PostService:
             media_upload_endpoint, auth=auth, files={"media": image}
         ).json()["media_id_string"]
 
-    def post(self, content, image):
+    def post_to_x(self, content, image):
 
         post = Post()
 
@@ -33,15 +33,37 @@ class PostService:
         media_id = self.fetch_media_id(auth, self.MEDIA_UPLOAD_ENDPOINT, image)
         x_post_payload = post.create_x_post_payload(content, media_id)
 
-        threads_auth = "Bearer " + os.environ["THREADS_ACCESS_TOKEN"]
-        threads_post_payload = post.create_threads_post_payload(content)
-
         try:
             x_response = requests.post(
                 self.POST_TWEET_ENDPOINT, auth=auth, json=x_post_payload
             )
             logger.info("【X API】メディアコンテナ作成リクエストのレスポンス : %s", x_response.json())
 
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 429:
+                logger.error(
+                    f"レート上限に達しました。: {e}",
+                    exc_info=True,
+                )
+        except requests.exceptions.RequestException as e:
+            logger.error(
+                f"リクエスト中にエラーが発生しました。: {e}",
+                exc_info=True,
+            )
+        except Exception as e:
+            logger.error(
+                f"投稿中に予期せぬエラーが発生しました。: {e}",
+                exc_info=True,
+            )
+
+    def post_to_threads(self, content):
+
+        post = Post()
+
+        threads_auth = "Bearer " + os.environ["THREADS_ACCESS_TOKEN"]
+        threads_post_payload = post.create_threads_post_payload(content)
+
+        try:
             threads_response = requests.post(
                 "https://graph.threads.net/v1.0/me/threads",
                 json=threads_post_payload,
